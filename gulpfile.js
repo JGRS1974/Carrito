0e3f33c4c43138cd9014d28180b3f14fb013c4d8
Jose Rodriguez <jgrs020174@gmail.com>
2020-09-02 21:48:24 -0300

    Header con formato

--- gulpfile.js
+++ gulpfile.js
@@ -15,5 +15,11 @@ function css() {
     .pipe( gulp.dest('css'));
 }
 
+function watchFile() {
+    gulp.watch('scss/*.scss', css);
+    gulp.watch('index.html');
+}
+
 //Registrar funciones como tareas
 gulp.task('css', css);
+gulp.task('watch', gulp.parallel(watchFile));
